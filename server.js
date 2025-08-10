00d91b1979047938bd35425f84382f0b24e4d831
VENKATESAN G <96653425+VenkatMastercoder@users.noreply.github.com>
2025-08-10 18:29:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Elastic Beanstalks - v1');
+  res.send('Hello from Elastic Beanstalk - v1');
 });
 
 app.get('/health', (req, res) => {
